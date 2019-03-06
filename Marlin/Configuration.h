fdac1a6675d03618b392bbe0374ebff14de25c69
William West <william.a.west@gmail.com>
2019-03-05 21:06:18 -0800

    Update PID params from 8-pass autocal@60C

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -401,9 +401,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define  DEFAULT_bedKp 199.56
-  #define  DEFAULT_bedKi 9.59
-  #define  DEFAULT_bedKd 1038.12
+  //#define  DEFAULT_bedKp 199.56
+  //#define  DEFAULT_bedKi 9.59
+  //#define  DEFAULT_bedKd 1038.12
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
@@ -412,6 +412,9 @@
   //#define  DEFAULT_bedKd 1675.16
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
+  #define  DEFAULT_bedKp 256.39
+  #define  DEFAULT_bedKi 14.56
+  #define  DEFAULT_bedKd 1128.93
 #endif // PIDTEMPBED
 
 // @section extruder
